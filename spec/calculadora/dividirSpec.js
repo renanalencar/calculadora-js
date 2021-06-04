70792e764f569afca5e723940f8f4df2751299ec
Renan Costa Alencar <renan.costaalencar@gmail.com>
2021-06-03 22:30:45 -0300

    Implementação da função de divisão

---
+++ spec/calculadora/dividirSpec.js
@@ -0,0 +1,21 @@
+describe("Suíte de testes de divisão", function() {
+
+    var Calculadora = require('../../src/js/calculadora.js');
+
+    it('deve retornar 2 para 6 e 3', function() {
+        expect(Calculadora.dividir(6, 3)).toEqual(2);
+    });
+    it('deve retornar 5 para 8 e 3 no formato texto', function() {
+        expect(Calculadora.dividir('6', '3')).toEqual(2);
+    });
+    it('deve retornar 1.5 para 4.5 e 3', function() {
+        expect(Calculadora.dividir(4.5, 3)).toEqual(1.5);
+        expect(Calculadora.dividir('4.5', '3')).toEqual(1.5);
+    });
+    it('deve retornar 0 quando o valor 1 não for numérico', function() {
+        expect(Calculadora.dividir(undefined, 5)).toEqual(0);
+    });
+    it('deve retornar 0 quando o valor 2 não for numérico', function() {
+        expect(Calculadora.dividir(5, undefined)).toEqual(0);
+    });
+})
--- src/js/calculadora.js
+++ src/js/calculadora.js
@@ -16,7 +16,15 @@ var Calculadora = {
         return num1 - num2;
     },
     dividir: function(num1, num2) {
-        return 0;
+        if (num2 == 0) {
+            return 'Erro';
+        }
+        if (isNaN(num1) || isNaN(num2)) {
+            return 0;
+        }
+        num1 = parseFloat(num1);
+        num2 = parseFloat(num2);
+        return num1 / num2;
     },
     multiplicar: function(num1, num2) {
         return 0;
