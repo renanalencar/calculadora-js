36b5ff3e8db106d0f53f28cf76607f8cdca74772
Renan Costa Alencar <renan.costaalencar@gmail.com>
2021-06-03 22:26:04 -0300

    Implementando a função de subtração

--- src/js/calculadora.js
+++ src/js/calculadora.js
@@ -8,7 +8,12 @@ var Calculadora = {
         return num1 + num2;
     },
     subtrair: function(num1, num2) {
-        return 0;
+        if (isNaN(num1) || isNaN(num2)) {
+            return 0;
+        }
+        num1 = parseFloat(num1);
+        num2 = parseFloat(num2);
+        return num1 - num2;
     },
     dividir: function(num1, num2) {
         return 0;
