1380ca7156707fd50d311f65f5afd8c14752f636
Renan Costa Alencar <renan.costaalencar@gmail.com>
2021-06-03 22:08:30 -0300

    Criando o objeto 'Calculadora'

---
+++ src/js/calcuadora.js
@@ -0,0 +1,18 @@
+var Calculadora = {
+    adicionar: function(num1, num2) {
+        return 0;
+    },
+    subtrair: function(num1, num2) {
+        return 0;
+    },
+    dividir: function(num1, num2) {
+        return 0;
+    },
+    multiplicar: function(num1, num2) {
+        return 0;
+    },
+};
+// usado na função 'require' do NodeJS
+if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+    module.exports = Calculadora;
+}
